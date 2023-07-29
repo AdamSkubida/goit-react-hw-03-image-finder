6baaa06a0a5d88c498bad8de96b4dcff61276848
AdamSkubida <adasiow.s@gmail.com>
2023-07-29 14:21:07 +0200

    SearchBar/form changed

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -12,27 +12,27 @@ export class SearchBar extends Component {
     this.setState({ query: value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = () => {
     const { query } = this.state;
-
-    e.preventDefault();
     this.props.onSubmit(query);
   };
 
   render() {
     return (
       <header className={css[`form-wrapper`]}>
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <input
-            className={css.input}
-            type="text"
-            placeholder="Search images and photos"
-            onChange={this.handleChange}
-          />
-          <button className={css.button} type="submit">
-            Submit
-          </button>
-        </form>
+        <input
+          className={css.input}
+          type="text"
+          placeholder="Search images and photos"
+          onChange={this.handleChange}
+        />
+        <button
+          className={css.button}
+          type="button"
+          onClick={this.handleSubmit}
+        >
+          Submit
+        </button>
       </header>
     );
   }
